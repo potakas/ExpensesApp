2b25959a241e50be8e0266af2babec949e18680e
potakas <panospotis@windowslive.com>
2022-12-01 11:49:53 +0200

    add functionality to Balance screen.

--- screens/Balance.js
+++ screens/Balance.js
@@ -1,22 +1,48 @@
-import { View,StyleSheet, Text } from "react-native"
+import { useContext } from "react";
+import { View, StyleSheet, Text, Dimensions } from "react-native";
+import BarChart from "react-native-bar-chart";
+import { GlobalStyles } from "../constants/styles";
+import { ExpensesContext } from "../store/expenses-context";
 
-const Balance= ()=>{
-    return <View style={styles.container}>
-        <Text>BALANCE</Text>
+
+const height = Dimensions.get("window").height;
+const width = Dimensions.get("window").width;
+const Balance = () => {
+  const expensesCtx = useContext(ExpensesContext);
+  // data can be one or two dimensional Array
+  const data = expensesCtx.expenses.map((expense) => expense.amount);
+  // labels
+  const horizontalData = expensesCtx.expenses.map((expense) =>
+    expense.date.toISOString().slice(5, 10)
+  );
+  //for better representation
+  const reverseData= data.reverse();
+  const reverseHorData= horizontalData.reverse();
+  return (
+    <View style={styles.container}>
+      <BarChart
+        data={reverseData}
+        horizontalData={reverseHorData}
+        barColor="red"
+        backgroundColor={GlobalStyles.colors.primary200}
+        barLabelColor="white"
+        labelColor="white"
+      />
     </View>
-}
+  );
+};
 
-export default Balance
+export default Balance;
 
 const styles = StyleSheet.create({
-    container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: 'white',
-	},
-	chart: {
-		width: 200,
-		height: 200,
-	},
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: GlobalStyles.colors.primary500,
+  },
+  chart: {
+    width: width,
+    height: height ,
+  },
+});
